751750ea626cb2ffcef89a21e5f59bc5650e97e0
andrea25-pc <paocen2017@gmail.com>
2025-01-28 23:18:34 -0500

    Add files via upload

---
+++ ejemplo3.py
@@ -0,0 +1,7 @@
+edad=int(input("Ingrese su edad: "))
+if 0<edad<=17:
+    print("Usted es menor de edad")
+elif edad<=0:
+    print("Ingrese una edad correcta")
+else:
+    print("Usted es mayor de edad")
---
+++ ejemplo5.py
@@ -0,0 +1,3 @@
+n=int(input("Ingrese un número entero: "))
+suma=n*(n+1)/2
+print(f"La suma de los {n} primeros números enteros es: ",suma)
